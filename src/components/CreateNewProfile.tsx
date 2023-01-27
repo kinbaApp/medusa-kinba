ef6d27ec934206791e155c6456f6517b4aa24d67
Sarah Azouvi <sarahazouvi@Sarahs-MacBook-Air.local>
2023-01-27 17:42:52 +0000

    minor aesthetic edits

--- src/components/CreateNewProfile.tsx
+++ src/components/CreateNewProfile.tsx
@@ -82,8 +82,8 @@ const CreateNewProfile: FC = () => {
 	}
 
 	return (
-		<div className="p-6  max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-			<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white truncate">
+		<div className="p-6  max-w-xl bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+			<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">
 				{'Click here to create a new profile '}
 			</h5>
 			{/* <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p> */}
--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -74,7 +74,7 @@ const Subscription: FC = () => {
 	return (
 		<div className="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
 			<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white truncate">
-				{'Subscribe !! '}
+				{'Subscribe to creator: '}
 			</h5>
 			{/* <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p> */}
 
