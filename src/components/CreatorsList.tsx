47d0a33e93ab6c41778b27f090606f84e2bf73b2
Sarah Azouvi <sarahazouvi@Sarahs-MacBook-Air.local>
2023-02-06 14:13:44 +0000

    fixed rendering issue

--- src/components/CreatorsList.tsx
+++ src/components/CreatorsList.tsx
@@ -11,17 +11,18 @@ const CreatorsList: FC = () => {
 	const creators = useGlobalStore(state => state.creators)
 	//const listOfCreators = creators.filter(creator => creator.)
 	const uniqueCreators = Array.from(new Set(creators))
+	const listCreators = uniqueCreators.map(creator => (
+		//<CreatorsSubscribedTo key={creator.toString()} {...creator} />
+
+		<li key={creator.creatorAddress}> {creator.creatorAddress.toString()}</li>
+	))
 
 	return (
 		<>
 			<h1 className="text-2xl font-mono font-light dark:text-white mt-10 mb-6">List of creators</h1>
 
-			<div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-4 w-full transition-all">
-				{uniqueCreators.map(creator => (
-					//<CreatorsSubscribedTo key={creator.toString()} {...creator} />
-					<li key={creator.creatorAddress}> {creator.creatorAddress}</li>
-				))}
-			</div>
+			<div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-4 w-full transition-all">{}</div>
+			<ul className="list-disc">{listCreators}</ul>
 		</>
 	)
 }
