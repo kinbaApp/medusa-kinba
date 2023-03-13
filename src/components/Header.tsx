54d51bb63d823cd701b458bacfdf330292f47339
Sarah Azouvi <sarahazouvi@Sarahs-MacBook-Air.local>
2023-03-13 23:17:28 +0000

    logo edit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -46,7 +46,7 @@ const Header: FC = () => {
 					<div className="flex justify-start lg:w-0 lg:flex-1">
 						<a href="https://medusanet.xyz">
 							<span className="sr-only">Medusa</span>
-							<img className="h-12 w-auto sm:h-16" src="/logo-white.gif" alt="" />
+							<img className="h-12 w-auto sm:h-16" src="/logo.png" alt="" />
 						</a>
 					</div>
 					<Link href="/">
