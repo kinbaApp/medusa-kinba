b763791c14d5d39d7a0ded71285fa9d33c826fba
Sarah Azouvi <sarahazouvi@Sarahs-MacBook-Air.local>
2023-03-16 13:45:09 +0000

    update userprofile component to look like profile page but adapted to each profile

--- src/components/reusable/Ad.tsx
+++ src/components/reusable/Ad.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../../../styles/reusable/Ad.module.scss'
 import PinkButton from './PinkButton'
 import fonts from '../../../styles/Fonts.module.scss'
+import Modal from 'react-modal'
 
-const Ad = ({ image, price }) => {
+const Ad = ({ image, price, period }) => {
 	// This entire component can be turned into a reusable
 	// component that each user can have displayed on their page.
 	// Made it veryyy minimal for now
+	const [isOpen, setIsOpen] = useState(false)
 
 	return (
 		<div className={styles.container}>
@@ -19,13 +21,17 @@ const Ad = ({ image, price }) => {
 					</div>
 				</div>
 				<div className={`${styles.offerDetails} ${fonts.lightText}`}>
-					Only {price} - Limited Time Only - <br />
-					Exclusive Nudes
+					Only {price} ETH - Limited Time Only - <br />
+					Exclusive Content
 				</div>
 			</div>
+
 			<PinkButton text={'SUBSCRIBE'} />
+
 			<div className={styles.bottomDetails}>
-				<p className={`${styles.bottomPrice} ${fonts.lightText}`}>{price} for 1 month</p>
+				<p className={`${styles.bottomPrice} ${fonts.lightText}`}>
+					{price} ETH for {period} days
+				</p>
 				<p className={`${styles.date} ${fonts.lightText}`}>February 24th 2023</p>
 			</div>
 		</div>
