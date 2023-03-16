684536b81d02c8cadf349fe575d5cbc37950886a
Batoul Alkarim <batoulalkarim1@gmail.com>
2023-03-16 12:00:19 -0400

    redid sign out button styling

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -4,37 +4,35 @@ import { Medusa } from '@medusa-network/medusa-sdk'
 import { ORACLE_ADDRESS } from '@/lib/consts'
 import { useSigner } from 'wagmi'
 import shallow from 'zustand/shallow'
-
+import styles from '../../styles/reusable/Signin.module.scss'
 
 const Signin: FC = () => {
-  const medusa = useGlobalStore((state) => state.medusa)
-  const updateMedusa = useGlobalStore((state) => state.updateMedusa)
-  const { data: signer } = useSigner()
-  console.log(medusa)
+	const medusa = useGlobalStore(state => state.medusa)
+	const updateMedusa = useGlobalStore(state => state.updateMedusa)
+	const { data: signer } = useSigner()
+	console.log(medusa)
 
-  const signMessage = async () => {
-    if (!signer) return
-    const medusa = await Medusa.init(ORACLE_ADDRESS, signer)
-    await medusa.signForKeypair()
-    updateMedusa(medusa)
-  }
+	const signMessage = async () => {
+		if (!signer) return
+		const medusa = await Medusa.init(ORACLE_ADDRESS, signer)
+		await medusa.signForKeypair()
+		updateMedusa(medusa)
+	}
 
-  console.log(medusa?.keypair)
-  if (medusa?.keypair) {
-    return <button
-      className="bg-gray-700 hover:bg-gray-500 hover:cursor-pointer text-gray-50 py-2 px-4 rounded"
-      onClick={() => medusa.setKeypair(null)}
-    >
-      Sign out
-    </button>
-  }
+	console.log(medusa?.keypair)
+	if (medusa?.keypair) {
+		return (
+			<button className={styles.signInButton} onClick={() => medusa.setKeypair(null)}>
+				Sign out
+			</button>
+		)
+	}
 
-  return <button
-    className="bg-gray-700 hover:bg-gray-500 hover:cursor-pointer text-gray-50 py-2 px-4 rounded"
-    onClick={() => signMessage()}
-  >
-    Sign in
-  </button>
+	return (
+		<div className={styles.signInButton} onClick={() => signMessage()}>
+			Sign in
+		</div>
+	)
 }
 
-export default Signin;
+export default Signin
